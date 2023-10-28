a99371fbe7dd3bd093cc2164a3152717e4c82811
nabilazh <102877599+nabilazh@users.noreply.github.com>
2023-10-28 21:43:54 +0800

    Delete Documents/MSIB Rakamin/week10 directory

--- Documents/MSIB Rakamin/week10/routes/movieRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const movieController = require('../controllers/movieController');
-
-router.get('/movies', movieController.getAllMovies);
-
-module.exports = router;
